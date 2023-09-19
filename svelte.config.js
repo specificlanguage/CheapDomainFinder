a827571228b15e0d33576f09aaad3ce9025a41d9
specificlanguage <specificlanguage@gmail.com>
2023-09-19 13:18:32 -0400

    chore: readd base URL

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,11 @@ const config = {
         adapter: adapter({
             edge: false,
             split: false
-        })
+        }),
+
+        paths: {
+            base: '/domains'
+        }
     }
 };
 
